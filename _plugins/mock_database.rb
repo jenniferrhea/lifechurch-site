897b270e4480174b935eadfcde2973496e32f215
Scott Lesser <scottwlesser@gmail.com>
2014-01-20 09:40:48 -0600

    made the filter more generic

--- _plugins/mock_database.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module MockDatabase
-    def find_series(data, series_id)
-      x = (data.select { |series| series["id"] == series_id }).first
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::MockDatabase)
