ec09835342e55749d7ced075c06fbed31834d754
Adam Fairhead <adam@adamfairhead.com>
2014-01-20 11:00:36 -0600

    Add 'more' panel functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+
+  // "More" panel open/close
+
+    
+    // Open the more panel
+    $( ".primary-links" ).delegate( ".nav-more", "click", function() {
+      $(".page-more").fadeIn();
+      $(".page-more-inner").animate({
+        top: '50px'
+      }, 500, function() {
+        // Animation complete.
+      });
+      
+      $(this).removeClass("nav-more").addClass("nav-close-more");
+      $(".nav-close-more").text("Close")
+    });
+
+    // Close the more panel
+    $( ".primary-links" ).delegate( ".nav-close-more", "click", function() {
+      $(".page-more").fadeOut();
+      $(".page-more-inner").animate({
+        top: '-100%'
+      }, 500, function() {
+        // Animation complete.
+      });
+
+      $(this).removeClass("nav-close-more").addClass("nav-more");
+      $(".nav-more").text("More")
+    });
+
+
+});
