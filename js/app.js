f7ab8a873b30d925222caf88780629b0cd24334d
Adam Fairhead <adam@adamfairhead.com>
2014-01-21 15:53:26 -0600

    Add Location map

--- js/app.js
+++ js/app.js
@@ -36,6 +36,9 @@ $(function() {
   // Add 'section-*' classes on <body>
 
     // For Locations
+    if ($('.page-locator:contains("Locations")').length > 0) {
+      $('body').addClass('page-locations');
+    };
     if ($('.section-locator:contains("Locations")').length > 0) {
       $('.section-locator').remove();
       $('body').addClass('section-locations');
@@ -52,5 +55,36 @@ $(function() {
       $('.section-locator').remove();
       $('body').addClass('section-giving');
     };
+
+
+  // Locations Map
+
+    // General pin zoom
+    $( ".map-wrapper" ).delegate( ".pin-ok, .pin-tx", "click", function() {
+      $(".pin-state").fadeOut(200);
+      $(".map-back").fadeIn(500);
+    });
+
+    // OK pin zoom
+    $( ".map-wrapper" ).delegate( ".pin-ok", "click", function() {
+      $(".map").addClass("map-ok");
+      $(".pin-campus-ok").delay(300).fadeIn(300);
+      $(".map-list-ok").delay(400).fadeIn(300);
+    });
+
+    // TX pin zoom
+    $( ".map-wrapper" ).delegate( ".pin-tx", "click", function() {
+      $(".map").addClass("map-tx");
+      $(".pin-campus-tx").delay(300).fadeIn(300);
+    });
+
+    // Back / Zoom out
+    $( ".map-wrapper" ).delegate( ".map-back", "click", function() {
+      $(".map").removeClass().addClass("map");
+      $(".pin, .map-list").fadeOut(200);
+      $(".map-back").fadeOut(500);
+      $(".pin-state").delay(300).fadeIn(300);
+    });
+
 
 });
