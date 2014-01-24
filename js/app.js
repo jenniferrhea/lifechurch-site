85095bfea13fd60ac38f9a611436e9efad148bc8
Adam Fairhead <adam@adamfairhead.com>
2014-01-24 15:27:42 -0600

    Add tools page content

--- js/app.js
+++ js/app.js
@@ -140,4 +140,12 @@ $(function() {
   });
 
 
+  // SVG / PNG
+  if(!Modernizr.svg) {
+    $('img[src*="svg"]').attr('src', function () {
+      return $(this).attr('src').replace('.svg', '.png');
+    });
+  }
+
+
 });
