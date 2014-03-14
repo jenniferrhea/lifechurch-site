5c46bb5ce367c4d77f4f1ad611d277c8a5155586
Adam Fairhead <adam@adamfairhead.com>
2014-03-14 15:07:59 -0500

    Update to counselor series page

--- js/watch/2014-the-counselor/script.js
+++ js/watch/2014-the-counselor/script.js
@@ -9,62 +9,64 @@ $(function() {
       // Fade out the intro text
       $(".slide-1").fadeOut(1000);
 
-      // Make canvas larger
-      $(".the-counselor-1").animate({
-        height: "700px"
-      }, 1000);
-
       // Hide nav
       $(".page-header, .page-tertiary").animate({ opacity: 0 }, 1000);
       // Move couch to the left
-      $(".couch").animate({ top: "390px", marginLeft: "-80px", opacity: 1 }, 1000, function() {
-        // Enter person 1
-        $(".animation-elements .person").addClass("person-1 state-1").animate({ opacity: 1, top: "170px" }, 500, function () {
-          // Person 1 state 2
-          $(".animation-elements .person").addClass("state-2").delay(50).animate({ opacity: 1 },0, function() {
+      $(".inner").animate({ opacity: 0 }, 1000, function() {
+        $(".couch").animate({ opacity: 1 }, 1000);
+        // Make canvas larger
+        $(".the-counselor-1").animate({ height: "700px" }, 1000, function () {
 
-            $(".animation-elements .person").addClass("state-3");
-            // Enter quote 1
-            $(".quote-1").fadeIn(1000, function() {
-              // Fade out person 1
-              $(".animation-elements .person-1").delay(2000).animate({ opacity: 0, }, 1000)
-              // Fade out quote 1
-              $(".quote-1").delay(2000).fadeOut(1000, function () {
-                $(".animation-elements .person").removeClass("person-1 state-2 state-3").animate({ top: "-130px" }, 0).addClass("person-2 state-1").animate({ opacity: 1, top: "170px" }, 500, function () {
-                  // Person 1 state 2
+          // Enter person 1
+          $(".animation-elements .person").addClass("person-1 state-1").animate({ opacity: 1, top: "170px" }, 500, function () {
+            // Person 1 state 2
+            $(".animation-elements .person").addClass("state-2").delay(50).animate({ opacity: 1 },0, function() {
 
-                  $(".animation-elements .person").addClass("state-2").delay(50).animate({ opacity: 1 },0, function() {
+              $(".animation-elements .person").addClass("state-3");
+              // Enter quote 1
+              $(".quote-1").fadeIn(1000, function() {
+                // Fade out person 1
+                $(".animation-elements .person-1").delay(2000).animate({ opacity: 0, }, 1000)
+                // Fade out quote 1
+                $(".quote-1").delay(2000).fadeOut(1000, function () {
+                  $(".animation-elements .person").removeClass("person-1 state-2 state-3").animate({ top: "-130px" }, 0).addClass("person-2 state-1").animate({ opacity: 1, top: "170px" }, 500, function () {
+                    // Person 1 state 2
 
-                    $(".animation-elements .person").addClass("state-3");
-                    // Enter quote 1
-                    $(".quote-2").fadeIn(1000, function() {
-                      // Fade out person 1
-                      $(".animation-elements .person-2").delay(2000).animate({ opacity: 0, }, 1000)
-                      // Fade out quote 1
-                      $(".quote-2").delay(2000).fadeOut(1000, function () {
-                      // Fade in quote 3
-                      $(".quote-3").fadeIn(1000, function() {
-                        // Fade out quote 3
-                        $(".quote-3").delay(2000).fadeOut(1000, function (){
-                          // Put everything back how it was
-                          $(".page-header, .page-tertiary").animate({ opacity: 1 }, 1000);
-                          $(".animation-elements .person").removeClass().addClass('person').animate({ top: "-130px" });
-                          $(".slide-1").fadeIn(1000);
-                          $(".couch").animate({ top: "270px", marginLeft: "0", opacity: .5 }, 1000);
-                          $(".the-counselor-1").animate({ height: "490px" }, 1000, function() {
-                            $('html, body').animate({ scrollTop:$('#messages').position().top }, 1000);
+                    $(".animation-elements .person").addClass("state-2").delay(50).animate({ opacity: 1 },0, function() {
+
+                      $(".animation-elements .person").addClass("state-3");
+                      // Enter quote 1
+                      $(".quote-2").fadeIn(1000, function() {
+                        // Fade out person 1
+                        $(".animation-elements .person-2").delay(2000).animate({ opacity: 0, }, 1000)
+                        // Fade out quote 1
+                        $(".quote-2").delay(2000).fadeOut(1000, function () {
+                        // Fade in quote 3
+                        $(".quote-3").fadeIn(1000, function() {
+                          // Fade out quote 3
+                          $(".quote-3").delay(2000).fadeOut(1000, function (){
+                            // Put everything back how it was
+                            $(".page-header, .page-tertiary").animate({ opacity: 1 }, 1000);
+                            $(".animation-elements .person").removeClass().addClass('person').animate({ top: "-130px" });
+                            $(".slide-1").fadeIn(1000);
+                            $(".couch").animate({ opacity: 0 }, 1000);
+                            $(".the-counselor-1").animate({ height: "490px" }, 1000, function() {
+                              $(".inner").animate({ opacity: 1 }, 1000);
+                              $('html, body').animate({ scrollTop:$('#messages').position().top }, 1000);
+                            });
                           });
                         });
                       });
-                    });
+                      });
                     });
                   });
                 });
               });
+
+
             });
-
-
           });
+
         });
       });
 
@@ -100,7 +102,7 @@ $(function() {
                         $(".page-header, .page-tertiary").animate({ opacity: 1 }, 1000);
                         $(".animation-elements .person").removeClass().addClass('person');
                         $(".slide-1").fadeIn(1000);
-                        $(".the-counselor-1").animate({ height: "350px" }, 1000, function(){
+                        $(".the-counselor-1").animate({ height: "415px" }, 1000, function(){
                           $('html, body').animate({ scrollTop:$('#messages').position().top }, 1000);
                         });
                       });
--- watch/_posts/2014-03-04-the-counselor.md
+++ watch/_posts/2014-03-04-the-counselor.md
@@ -14,28 +14,29 @@ skrollable: false
 
 <div class="page-section section-first section-light">
 <div class="the-counselor-1 contain">
+<div class="inner"></div>
 
   <section class="slide-1">
     <div class="row">
-      <div class="medium-7 medium-centered columns medium-text-center">
+      <div class="medium-7 columns">
         <h1>{{ series.title }}</h1>
         <p>{{ series.description_medium }}</p>
         <p>
 
           {% for message in series.messages limit:1 %}
           <a href="javascript:void(0);"
-             class="action"
+             class=""
              data-video-player="{{message.video}}"
              >
              Watch Church Online in 14min 28sec <i class="icon icon-play-2"></i>
           </a>
           {% endfor %}
 
-
+<br />
           <a href="#"
-             class="action learn-more"
+             class="learn-more"
              >
-             Learn More <i class="icon icon-play-1"></i>
+             Learn More <i class="icon icon-play-2"></i>
           </a>
         </p>
       </div>
